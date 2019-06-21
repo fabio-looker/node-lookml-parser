8cc71dc9e57641dcb502997c1947703a6c26a014
jax <jax@looker.com>
2019-06-21 16:17:47 +0100

    add helper function to access parsed object

--- parse/index.js
+++ parse/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable require-jsdoc */
 !function(){
 		const peg = require("pegjs")
 		const path = require("path")
@@ -23,14 +24,38 @@
 					stringToParse = "model: "+model+" {\n"+stringToParse+"\n}\n";
 				}
 				try{
-				return lookmlParser.parse(stringToParse)
+					parseContents = lookmlParser.parse(stringToParse);
+					recurse(parseContents, value => {
+						if(['dimension', 'measure', 'filter', 'dimension_group', 'parameter'].includes(value._type)) {
+							references = []
+							if (references.length) { 
+								value._references = []
+								// insert logic here
+							}
+						}
+					})
+
+				// 	})
+					return parseContents
 				}catch(e){
 					throw {	toString:()=>"Parse error@"+(e.location && e.location.start.line+","+e.location.start.column)+" "+e.message,
 							...e, 
 							...(e.location?{
 								context:stringToParse.split("\n").map((l,i)=>''+(i+1)+":	"+l).slice(e.location.start.line-4,e.location.end.line+2).join("\n")
 								}:{})
-							}
+							} 
 				}
 			}
-	}()
+}()
+
+function recurse(obj, fn) {
+	fn(obj);
+
+	if (typeof(obj) !== 'object') {
+		return;
+	}
+
+	for (const val of Object.values(obj)) {
+		recurse(val, fn);
+	}
+};
