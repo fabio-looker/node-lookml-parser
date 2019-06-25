988418ff0ec7b846a03d0758c7a845087a742a57
jax <jax@looker.com>
2019-06-25 21:10:57 +0100

    all possible LookML parameters for dims/measures

--- parse/index.js
+++ parse/index.js
@@ -26,28 +26,49 @@
 				try{
 					parseContents = lookmlParser.parse(stringToParse);
 					recurse(parseContents, value => {
-						if(['dimension', 'measure'].includes(value._type)) {
+						if(['dimension', 'measure', 'dimension_group', 'filter', 'filters'].includes(value._type)) {
 							references = []
 
 						/* Dimensions:
 							sql: ${view.field} & ${field}  & {% parameter PARAMETER_NAME %} & 
-							html: view.
+							html: {{_field._name}} & {{ _access_filters['company.name'] }} & {{ _filters['view.field'] }} 
 							drill_fields: [field | view.field]
+							start_location_field:
+							end_location_field:
+							label_from_parameter:
+							order_by_field:
+							required_fields:
+							sql_end:
+							sql_start:
+							sql_longitude:
+							sql_latitude:
+							suggest_dimension:
 
-							Measures:
+						  Measures:
 							label_from_parameter: view.parameter | parameter
 							sql: sql: ${view.field} & ${field}  & {% parameter PARAMETER_NAME %} 
 							drill_fields: [field | view.field]
+							filters: { field: } 
+							html:
+							list_field
+							required_fields
+							suggest_dimension
 
-							TBD: 'filter', 'dimension_group', 'parameter'
+
+							TBD: 'parameter'
 						*/
 						value._references = [];
-						[value.sql, value.html].forEach((param) => {
+						[value.sql,
+							value.sql_start,
+							value.sql_end,
+							value.sql_longitude,
+							value.sql_latitude,
+							value.html].forEach((param) => {
 							if (!param) { 
 								return;
 							} else {
 								let match = param
-									.replace(/\b\d+\.\d+\b/g, '') 
+									.replace(/\b\d+\.\d+\b/g, '')
 									.match(/(^|\$\{|\{\{|\{%)\s*(([^.{}]+)(\.[^.{}]+)+)\s*($|%\}|\})/);
 								
 								// Ensure a string is always returned, even if there are no matches
@@ -55,12 +76,33 @@
 
 								// If nothing matches, move on. Otherwise add reference to _references
 								if (!parts.length) {
-									console.log(`No references found in ${param}`);
+									return;
 								} else {
-									value._references.push(parts)
+									value._references.push(parts);
 								}
 							}
 						});
+						
+						// need to match:
+						[value.drill_fields, 
+							value.label_from_parameter, // field | view.field
+							value.start_location_field, // field | view.field
+							value.end_location_field, // field | view.field
+							value.suggest_dimension, // field 
+							value.order_by_field, // field
+							value.required_fields, // [field1, field2, set*] // TODO: address use case where a set is passed in.
+							value.list_field, // field
+							value.field // in filtered measures. field | view.field  
+							].forEach((param) => {
+								if (!param) {
+									// Case when LookML parameter does not exist
+									return;
+								} else if (Array.isArray(param)) {
+									// Case when LookML parameter contains a list
+								} else {
+									// Case when LookML parameter contains just one string
+								}
+							})
 					}
 				})
 					return parseContents
